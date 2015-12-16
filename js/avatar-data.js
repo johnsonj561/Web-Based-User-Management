113e7c7003878c39253ccb2f8e39c798306353f2
Justin Johnson <johnsonj561@gmail.com>
2015-12-15 19:43:15 -0500

    DDSlick added to manage avatar drop down menu, avatar-data stores json object of all avatars

---
+++ js/avatar-data.js
@@ -0,0 +1,352 @@
+var avatarData = [
+  {
+    value: 1,
+    selected: true,
+    imageSrc: "../img/avatars/avt-1.png"
+  },
+  {
+    value: 2,
+    selected: false,
+    imageSrc: "../img/avatars/avt-2.png"
+  },
+  {
+    value: 3,
+    selected: false,
+    imageSrc: "../img/avatars/avt-3.png"
+  },
+  {
+    value: 4,
+    selected: false,
+    imageSrc: "../img/avatars/avt-4.png"
+  },
+  {
+    value: 5,
+    selected: false,
+    imageSrc: "../img/avatars/avt-5.png"
+  },
+  {
+    value: 6,
+    selected: false,
+    imageSrc: "../img/avatars/avt-6.png"
+  },
+  {
+    value: 7,
+    selected: false,
+    imageSrc: "../img/avatars/avt-7.png"
+  },
+  {
+    value: 8,
+    selected: false,
+    imageSrc: "../img/avatars/avt-8.png"
+  },
+  {
+    value: 9,
+    selected: false,
+    imageSrc: "../img/avatars/avt-9.png"
+  },
+  {
+    value: 10,
+    selected: false,
+    imageSrc: "../img/avatars/avt-10.png"
+  },
+  {
+    value: 11,
+    selected: false,
+    imageSrc: "../img/avatars/avt-11.png"
+  },
+  {
+    value: 12,
+    selected: false,
+    imageSrc: "../img/avatars/avt-12.png"
+  },
+  {
+    value: 13,
+    selected: false,
+    imageSrc: "../img/avatars/avt-13.png"
+  },
+  {
+    value: 14,
+    selected: false,
+    imageSrc: "../img/avatars/avt-14.png"
+  },
+  {
+    value: 15,
+    selected: false,
+    imageSrc: "../img/avatars/avt-15.png"
+  },
+  {
+    value: 16,
+    selected: false,
+    imageSrc: "../img/avatars/avt-16.png"
+  },
+  {
+    value: 17,
+    selected: false,
+    imageSrc: "../img/avatars/avt-17.png"
+  },
+  {
+    value: 18,
+    selected: false,
+    imageSrc: "../img/avatars/avt-18.png"
+  },
+  {
+    value: 19,
+    selected: false,
+    imageSrc: "../img/avatars/avt-19.png"
+  },
+  {
+    value: 20,
+    selected: false,
+    imageSrc: "../img/avatars/avt-20.png"
+  },
+  {
+    value: 21,
+    selected: false,
+    imageSrc: "../img/avatars/avt-21.png"
+  },
+  {
+    value: 22,
+    selected: false,
+    imageSrc: "../img/avatars/avt-22.png"
+  },
+  {
+    value: 23,
+    selected: false,
+    imageSrc: "../img/avatars/avt-23.png"
+  },
+  {
+    value: 24,
+    selected: false,
+    imageSrc: "../img/avatars/avt-24.png"
+  },
+  {
+    value: 25,
+    selected: false,
+    imageSrc: "../img/avatars/avt-25.png"
+  },
+  {
+    value: 26,
+    selected: false,
+    imageSrc: "../img/avatars/avt-26.png"
+  },
+  {
+    value: 27,
+    selected: false,
+    imageSrc: "../img/avatars/avt-27.png"
+  },
+  {
+    value: 28,
+    selected: false,
+    imageSrc: "../img/avatars/avt-28.png"
+  },
+  {
+    value: 29,
+    selected: false,
+    imageSrc: "../img/avatars/avt-29.png"
+  },
+  {
+    value: 30,
+    selected: false,
+    imageSrc: "../img/avatars/avt-30.png"
+  },
+  {
+    value: 31,
+    selected: false,
+    imageSrc: "../img/avatars/avt-31.png"
+  },
+  {
+    value: 32,
+    selected: false,
+    imageSrc: "../img/avatars/avt-32.png"
+  },
+  {
+    value: 33,
+    selected: false,
+    imageSrc: "../img/avatars/avt-33.png"
+  },
+  {
+    value: 34,
+    selected: false,
+    imageSrc: "../img/avatars/avt-34.png"
+  },
+  {
+    value: 35,
+    selected: false,
+    imageSrc: "../img/avatars/avt-35.png"
+  },
+  {
+    value: 36,
+    selected: false,
+    imageSrc: "../img/avatars/avt-36.png"
+  },
+  {
+    value: 37,
+    selected: false,
+    imageSrc: "../img/avatars/avt-37.png"
+  },
+  {
+    value: 38,
+    selected: false,
+    imageSrc: "../img/avatars/avt-38.png"
+  },
+  {
+    value: 39,
+    selected: false,
+    imageSrc: "../img/avatars/avt-39.png"
+  },
+  {
+    value: 40,
+    selected: false,
+    imageSrc: "../img/avatars/avt-40.png"
+  },
+  {
+    value: 41,
+    selected: false,
+    imageSrc: "../img/avatars/avt-41.png"
+  },
+  {
+    value: 42,
+    selected: false,
+    imageSrc: "../img/avatars/avt-42.png"
+  },
+  {
+    value: 43,
+    selected: false,
+    imageSrc: "../img/avatars/avt-43.png"
+  },
+  {
+    value: 44,
+    selected: false,
+    imageSrc: "../img/avatars/avt-44.png"
+  },
+  {
+    value: 45,
+    selected: false,
+    imageSrc: "../img/avatars/avt-45.png"
+  },
+  {
+    value: 46,
+    selected: false,
+    imageSrc: "../img/avatars/avt-46.png"
+  },
+  {
+    value: 47,
+    selected: false,
+    imageSrc: "../img/avatars/avt-47.png"
+  },
+  {
+    value: 48,
+    selected: false,
+    imageSrc: "../img/avatars/avt-48.png"
+  },
+  {
+    value: 49,
+    selected: false,
+    imageSrc: "../img/avatars/avt-49.png"
+  },
+  {
+    value: 50,
+    selected: false,
+    imageSrc: "../img/avatars/avt-50.png"
+  },
+  {
+    value: 51,
+    selected: false,
+    imageSrc: "../img/avatars/avt-51.png"
+  },
+  {
+    value: 52,
+    selected: false,
+    imageSrc: "../img/avatars/avt-52.png"
+  },
+  {
+    value: 53,
+    selected: false,
+    imageSrc: "../img/avatars/avt-53.png"
+  },
+  {
+    value: 54,
+    selected: false,
+    imageSrc: "../img/avatars/avt-54.png"
+  },
+  {
+    value: 55,
+    selected: false,
+    imageSrc: "../img/avatars/avt-55.png"
+  },
+  {
+    value: 56,
+    selected: false,
+    imageSrc: "../img/avatars/avt-56.png"
+  },
+  {
+    value: 57,
+    selected: false,
+    imageSrc: "../img/avatars/avt-57.png"
+  },
+  {
+    value: 58,
+    selected: false,
+    imageSrc: "../img/avatars/avt-58.png"
+  },
+  {
+    value: 59,
+    selected: false,
+    imageSrc: "../img/avatars/avt-59.png"
+  },
+  {
+    value: 60,
+    selected: false,
+    imageSrc: "../img/avatars/avt-60.png"
+  },
+    {
+    value: 61,
+    selected: false,
+    imageSrc: "../img/avatars/avt-61.png"
+  },
+  {
+    value: 62,
+    selected: false,
+    imageSrc: "../img/avatars/avt-62.png"
+  },
+  {
+    value: 63,
+    selected: false,
+    imageSrc: "../img/avatars/avt-63.png"
+  },
+  {
+    value: 64,
+    selected: false,
+    imageSrc: "../img/avatars/avt-64.png"
+  },
+  {
+    value: 65,
+    selected: false,
+    imageSrc: "../img/avatars/avt-65.png"
+  },
+  {
+    value: 66,
+    selected: false,
+    imageSrc: "../img/avatars/avt-66.png"
+  },
+  {
+    value: 67,
+    selected: false,
+    imageSrc: "../img/avatars/avt-67.png"
+  },
+  {
+    value: 68,
+    selected: false,
+    imageSrc: "../img/avatars/avt-68.png"
+  },
+  {
+    value: 69,
+    selected: false,
+    imageSrc: "../img/avatars/avt-69.png"
+  },
+  {
+    value: 70,
+    selected: false,
+    imageSrc: "../img/avatars/avt-70.png"
+  }
+];
